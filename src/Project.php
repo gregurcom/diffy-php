03dad3505b4c1ea2195c1f54cbea7d641dea2ccf
Yuriy Gerasimov <yuri.gerasimov@gmail.com>
2020-02-24 18:58:47 -0800

    Add compare urls arguments.

--- src/Project.php
+++ src/Project.php
@@ -4,7 +4,7 @@ namespace Diffy;
 
 class Project {
 
-  static $ENVIRONMENTS = ['prod', 'stage', 'dev', 'baseline'];
+  static $ENVIRONMENTS = ['prod', 'stage', 'dev', 'baseline', 'custom'];
 
   /**
    * Get list of all Projects.
@@ -25,16 +25,31 @@ class Project {
       throw new InvalidArgumentsException('Compare call requires "env2" as the second environment to compare.');
     }
     if (!in_array($params['env1'], self::$ENVIRONMENTS)) {
-      throw new InvalidArgumentsException('"env1" is not a valid environment. Can be one of: prod, stage, dev, baseline');
+      throw new InvalidArgumentsException('"env1" is not a valid environment. Can be one of: prod, stage, dev, baseline, custom');
     }
     if (!in_array($params['env2'], self::$ENVIRONMENTS)) {
-      throw new InvalidArgumentsException('"env2" is not a valid environment. Can be one of: prod, stage, dev, baseline');
+      throw new InvalidArgumentsException('"env2" is not a valid environment. Can be one of: prod, stage, dev, baseline, custom');
+    }
+    if ($params['env1'] == 'custom' && !isset($params['env1Url'])) {
+      throw new InvalidArgumentsException('"env1" is Custom but you did not provide URl. Please provide it in env1Url variable');
+    }
+    if ($params['env2'] == 'custom' && !isset($params['env2Url'])) {
+      throw new InvalidArgumentsException('"env2" is Custom but you did not provide URl. Please provide it in env2Url variable');
     }
 
-    return Diffy::request('POST', 'projects/' . $projectId . '/compare', [
+    $arguments = [
       'env1' => $params['env1'],
       'env2' => $params['env2']
-    ]);
+    ];
+
+    if ($params['env1'] == 'custom') {
+      $arguments['env1Url'] = $params['env1Url'];
+    }
+    if ($params['env2'] == 'custom') {
+      $arguments['env2Url'] = $params['env2Url'];
+    }
+
+    return Diffy::request('POST', 'projects/' . $projectId . '/compare', $arguments);
   }
 
 }
